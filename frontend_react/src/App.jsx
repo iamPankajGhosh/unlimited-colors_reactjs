27375f2d94dac1c326bbd51df0d20878d0b1e41d
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-01-21 13:39:16 +0530

    project done

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
 function App() {
-  return <div>App</div>;
+  const [isChanging, setIsChanging] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isChanging) {
+      intervalId = setInterval(() => {
+        let randomColor = "#";
+        let colorCode = "0123456789abcdefABCDEF";
+
+        for (let i = 0; i < 6; i++) {
+          randomColor += colorCode[Math.floor(Math.random() * 16)];
+        }
+
+        document.body.style.backgroundColor = randomColor;
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isChanging]);
+
+  return (
+    <>
+      <div className="container">
+        <h1>Unlimited Colors</h1>
+        <div className="btns">
+          <button
+            className="btn"
+            id="start"
+            onClick={() => setIsChanging(true)}
+          >
+            Start
+          </button>
+          <button
+            className="btn"
+            id="stop"
+            onClick={() => setIsChanging(false)}
+          >
+            Stop
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
